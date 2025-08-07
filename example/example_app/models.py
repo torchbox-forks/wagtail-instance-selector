3ad869ad4039c3f71895a3d2a8cc916090062681
nickmoreton <nickmoreton@me.com>
2025-08-07 12:03:47 +0100

    Reproduce the inline panel issue

--- example/example_app/models.py
+++ example/example_app/models.py
@@ -3,10 +3,12 @@ from django.db import models
 from instance_selector.edit_handlers import InstanceSelectorPanel
 from instance_selector.blocks import InstanceSelectorBlock
 
-from wagtail.admin.panels import FieldPanel
-from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.models import Page, Orderable
 from wagtail.fields import StreamField
 from wagtail.blocks import CharBlock, RichTextBlock
+
+from modelcluster.fields import ParentalKey
 
 
 class Shop(models.Model):
@@ -61,5 +63,29 @@ class ShopPage(Page):
     ])
 
     content_panels = Page.content_panels + [
+        InlinePanel("authors", label="Authors"),
         FieldPanel("content"),
     ]
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel("name"),
+    ]
+
+    def __str__(self):
+        return self.name
+    
+
+class ShopPageAuthor(Orderable):
+    page = ParentalKey(ShopPage, related_name="authors")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="shop_pages")
+
+    panels = [
+        InstanceSelectorPanel("author"),
+    ]
+
+    def __str__(self):
+        return str(self.author)
